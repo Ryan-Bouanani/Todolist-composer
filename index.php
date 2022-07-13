0716fcdeb28f3fe9a0044d64ed0c392f5c3914c3
Ryan-Bouanani <ryan42426@gmail.com>
2022-07-13 09:47:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <ul>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Perspiciatis suscipit aspernatur pariatur consequatur!</li>
+        <li>Vero architecto et facilis porro.</li>
+        <li>Quidem excepturi doloribus voluptas itaque.</li>
+        <li>Facilis minima eligendi est molestias.</li>
+    </ul>
+
+</body>
+</html>
